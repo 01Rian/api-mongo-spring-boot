b6db0d3c439ba638080631b0580e4b14f43eb482
Rian <riansantosjr11@gmail.com>
2024-02-10 19:44:43 -0300

    add user and role repositories

---
+++ src/main/java/com/exampleAPI/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.exampleAPI.repositories;
+
+import com.exampleAPI.entities.Role;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends MongoRepository<Role, String> {
+}
